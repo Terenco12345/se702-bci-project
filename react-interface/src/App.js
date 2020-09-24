72df7b5c600973b9d4c1055b8aa9815f6c54a538
Terence <terenco12345@gmail.com>
2020-09-24 21:15:39 +1200

    Adding project

---
+++ react-interface/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the experiment!</h1>
+      <p>blah blah blah</p>
+    </div>
+  );
+}
+
+export default App;
